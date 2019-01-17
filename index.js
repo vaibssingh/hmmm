ef8d70857c58647015bd061d08f53a9135b0ab57
Vaibhav <singhvaibhav592@gmail.com>
2019-01-18 02:16:57 +0530

    error handling and other updates

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const harakiri = require('harakiri');
 const fs = require('fs');
 const Trie = require('./trie.js');
-const app = express()
+const app = express();
 const port = 5000
 
 app.get('/', (req, res) => {
@@ -15,49 +16,67 @@ app.use(bodyParser.urlencoded({
 
 app.post('/insert', (req, res) => {
     let words = req.body.inputField;
-    if (!/[a-z]+(\s[a-z]+)*/.test(words)) { //checking for invalid input
+    const pattern = /[a-z]+(\s[a-z]+)*$/;
+    if (!pattern.test(words)) { //checking for invalid input
         res.write('Please go back and insert list of words first')
         res.end();
     } else {
         let names = words.split(" ");
-        createTree(names);                  // creating the Trie from the array of words
+        createTree(names); // creating the Trie from the array of words
         res.sendFile(__dirname + '/find.html');
     }
 });
 
 // Path to show result of matched words
 app.post('/find', (req, res) => {
-    let list = findWords(req.body.name); //returned list of matched words
+    let list = findWords(req.body.name); // returned list of matched words
     if (list.length >= 1) {
-        res.send(list)                   // currently sending the whole array itself
+        res.send(list) // currently sending the whole array itself
     } else {
         res.send('No matches found. Why not go back and try again?')
     }
 });
 
+// Error handling for Express
 app.use((err, res) => {
-    console.error(err.stack);
-    fs.appendFile('error.txt', err.stack, (err) => {        // logging errors in a file
+    fs.appendFile('error.txt', err.stack, err => { // logging errors in a file
         if (err) throw err;
     })
     res.status(500).send('Oops! Something went wrong ¯\_(ツ)_/¯. It\'s probably not your fault tho!');
 });
 
 // Instantiating the Trie fn
-let T = new Trie();
+const T = new Trie();
 
 // Function to create Trie tree
 const createTree = array => {
-    for (let i = 0; i < array.length; i++) {
-        T.insert(array[i]);
+    try {
+        for (let i = 0; i < array.length; i++) {
+            T.insert(array[i]);
+        }
+    } catch (error) {
+        console.error(error)
+        fs.appendFile('error.txt', error, err => {
+            if (err) throw err;
+        })
     }
 };
 
 // Function to find words from Trie
 const findWords = str => {
-    let list = T.autoComplete(str);
-    return list;
+    try {
+        let list = T.autoComplete(str);
+        return list;
+    } catch (error) {
+        console.error(error);
+        fs.appendFile('error.txt', error, err => {
+            if (err) throw err;
+        })
+    }
 };
 
 // Server init
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => { 
+    harakiri();
+    console.log(`Example app listening on port ${port}!`)
+});
